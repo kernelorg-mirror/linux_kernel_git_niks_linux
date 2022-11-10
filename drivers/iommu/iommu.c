b205879997b5745420ba49a26fb5a0c0e3d5a4f9
Niklas Schnelle <schnelle@linux.ibm.com>
2022-11-10 12:41:34 +0100

    iommu: Let iommu.strict override ops->def_domain_type
    When iommu.strict=1 is set or iommu_set_dma_strict() was called we
    should use IOMMU_DOMAIN_DMA irrespective of ops->def_domain_type.
    
    Signed-off-by: Niklas Schnelle <schnelle@linux.ibm.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -1562,6 +1562,9 @@ static int iommu_get_def_domain_type(struct device *dev)
 {
 	const struct iommu_ops *ops = dev_iommu_ops(dev);
 
+	if (iommu_dma_strict)
+		return IOMMU_DOMAIN_DMA;
+
 	if (dev_is_pci(dev) && to_pci_dev(dev)->untrusted)
 		return IOMMU_DOMAIN_DMA;
 
