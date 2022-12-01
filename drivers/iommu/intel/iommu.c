a2aecd821fe3c5e2549946a68d8b07e05b288a9b
Niklas Schnelle <schnelle@linux.ibm.com>
2022-12-01 13:06:33 +0100

    iommu: Allow .iotlb_sync_map to fail and handle s390's -ENOMEM return
    On s390 .iotlb_sync_map is used to sync mappings to an underlying
    hypervisor by letting the hypervisor inspect the synced IOVA range and
    updating its shadow table. This however means that it can fail as the
    hypervisor may run out of resources. This can be due to a limit on concurrently
    mapped addresses such as vfio_iommu_type1.dma_entry_limit on KVM or it can
    be a failure to pin guest pages. Either way such a failure to sync
    a mapping should result in a DMA_MAPPING_EROR.
    
    Now especially when running with batched IOTLB flushes for unmap it may
    be that some IOVAs have already been invalidated but not yet flushed.
    Thus if the RPCIT indicates running out of resources first do a global
    flush allowing the hypervisor to free resources for these mappings
    and only if that also fails report this error upwards.
    
    Signed-off-by: Niklas Schnelle <schnelle@linux.ibm.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -4699,8 +4699,8 @@ static bool risky_device(struct pci_dev *pdev)
 	return false;
 }
 
-static void intel_iommu_iotlb_sync_map(struct iommu_domain *domain,
-				       unsigned long iova, size_t size)
+static int intel_iommu_iotlb_sync_map(struct iommu_domain *domain,
+				      unsigned long iova, size_t size)
 {
 	struct dmar_domain *dmar_domain = to_dmar_domain(domain);
 	unsigned long pages = aligned_nrpages(iova, size);
@@ -4710,6 +4710,7 @@ static void intel_iommu_iotlb_sync_map(struct iommu_domain *domain,
 
 	xa_for_each(&dmar_domain->iommu_array, i, info)
 		__mapping_notify_one(info->iommu, dmar_domain, pfn, pages);
+	return 0;
 }
 
 const struct iommu_ops intel_iommu_ops = {
