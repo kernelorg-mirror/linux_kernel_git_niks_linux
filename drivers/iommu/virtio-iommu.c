c705997f02ddc08b1d9fca89c0105c80a7d06397
Niklas Schnelle <schnelle@linux.ibm.com>
2023-08-25 13:02:38 +0200

    iommu/virtio: Add ops->flush_iotlb_all and enable deferred flush
    Add ops->flush_iotlb_all operation to enable virtio-iommu for the
    dma-iommu deferred flush scheme. This results inn a significant increase
    in performance in exchange for a window in which devices can still
    access previously IOMMU mapped memory. To get back to the prior behavior
    iommu.strict=1 may be set on the kernel command line.
    
    Link: https://lore.kernel.org/lkml/20230802123612.GA6142@myrica/
    Signed-off-by: Niklas Schnelle <schnelle@linux.ibm.com>

--- drivers/iommu/virtio-iommu.c
+++ drivers/iommu/virtio-iommu.c
@@ -924,6 +924,15 @@ static int viommu_iotlb_sync_map(struct iommu_domain *domain,
 	return viommu_sync_req(vdomain->viommu);
 }
 
+static void viommu_flush_iotlb_all(struct iommu_domain *domain)
+{
+	struct viommu_domain *vdomain = to_viommu_domain(domain);
+
+	if (!vdomain->nr_endpoints)
+		return;
+	viommu_sync_req(vdomain->viommu);
+}
+
 static void viommu_get_resv_regions(struct device *dev, struct list_head *head)
 {
 	struct iommu_resv_region *entry, *new_entry, *msi = NULL;
@@ -1049,6 +1058,8 @@ static bool viommu_capable(struct device *dev, enum iommu_cap cap)
 	switch (cap) {
 	case IOMMU_CAP_CACHE_COHERENCY:
 		return true;
+	case IOMMU_CAP_DEFERRED_FLUSH:
+		return true;
 	default:
 		return false;
 	}
@@ -1069,6 +1080,7 @@ static struct iommu_ops viommu_ops = {
 		.map_pages		= viommu_map_pages,
 		.unmap_pages		= viommu_unmap_pages,
 		.iova_to_phys		= viommu_iova_to_phys,
+		.flush_iotlb_all	= viommu_flush_iotlb_all,
 		.iotlb_sync		= viommu_iotlb_sync,
 		.iotlb_sync_map		= viommu_iotlb_sync_map,
 		.free			= viommu_domain_free,
